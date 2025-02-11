f23b2d98ffc1fc1df67ff47a7ce7418f922726cd
student <student@ENGR8RJD5M3.ad.snow.edu>
2025-02-11 08:21:30 -0700

    Finished Step 2

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+//Step 1//
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
+
+c = a - b;
+Console.WriteLine(c);
+
+c = a * b;
+Console.WriteLine(c);
+
+c = a / b;
+Console.WriteLine(c);
+
+//Step 2//
+a = 5;
+b = 4;
+c = 2;
+int d = a + b * c;
+Console.WriteLine(d);
+
+d = (a + b) * c;
+Console.WriteLine(d);
+
+d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+Console.WriteLine(d);
+
+d = (a + b) / c;
+Console.WriteLine(d);
